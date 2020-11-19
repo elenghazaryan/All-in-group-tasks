5aa2318880096dd113999543f0103004335e0c42
elenghazaryan <kelen_2000@mail.ru>
2020-11-19 19:16:22 +0400

    last commit

---
+++ ToDo_Project/todo/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'todo'
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('todo/create', views.create, name='create'),
+    path('<int:id>/delete', views.delete, name='delete'),
+]
