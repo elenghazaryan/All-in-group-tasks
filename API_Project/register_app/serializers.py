5162bffb04c50b76fd0cf447d4947bbd0830e9bb
elenghazaryan <kelen_2000@mail.ru>
2020-12-08 11:22:46 +0400

    hashed password

--- API_Project/register_app/serializers.py
+++ API_Project/register_app/serializers.py
@@ -1,8 +1,12 @@
 from .models import User
 from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'username', 'password', 'email']
+
+    def validate_password(self, value):
+        return make_password(value)
